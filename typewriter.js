80fd425801235bff8e739e26c7d9ad8c892c316c
Farid Asadpour <yani5752000@yahoo.com>
2021-06-29 18:11:34 +0000

    typewriter

--- typewriter.js
+++ typewriter.js
@@ -1,18 +1,14 @@
 const sentence = "hello there from lighthouse labs";
 
 let delay = 0;
-//let i = 0;
-for (const char of sentence) {
-  //process.stdout.write(char);
-
-  
+for (const char of sentence) { 
   setTimeout(() => {
     process.stdout.write(char);// print the char here
   }, delay); // <= 1s delay to make it noticeable. Can dial it down later.
 
   delay += 50;
 }
-//console.log("\n");
+
 setTimeout(() => {
-  console.log("");// print the char here
+  console.log("");
 }, delay);
